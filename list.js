96f822eeac05f83cca141d1de0253a36024aec57
Johnny Walkeray <johnny.walkeray@gmail.com>
2018-05-18 16:21:50 +0300

    Add files

---
+++ list.js
@@ -0,0 +1,39 @@
+
+redpill = [	'Religion and science are not mutually exclusive.', 
+			'Atheists claim that their “lack of belief” has no ethical ramifications, but they are wrong.',
+			'Communists must be atheistic because the state must supplant God as the supreme entity.',
+			'Christ did not explicitly condemn slavery, but the Golden Rule “Do unto others as you would have them do unto you,” makes it rather clear what He wanted people to do.',
+			'Hebrew law ordered Jews to treat slaves as family and the Jew who killed a slave was to die, just as they would for killing a fellow Jew.',
+			'Christianity and Judaism both demand humane treatment of others. Christianity demands it regardless of race, class, sex, etc.',
+			'To judge all religious people by a single incident or period of a single sect is patently ridiculous.',
+			'The atheist initiative to drive religion out of any and every public or government space is in direct conflict with the portion of the First Amendment.',
+			'Claiming the definition of atheism to be merely ‘lack of belief’ is obfuscation as the definition has traditionally been belief that no God or gods exist/active denial of God’s existence.',
+			'Galileo was not burned.',
+			'Jesus Christ is not a myth.',
+			'Hitler was not a Christian. He was not an atheist, either.',
+			'The Earth is NOT flat.',
+			'New Atheism: claiming that your feelings are above all the scientific facts that invalidate your personal opinion.',
+			'"Liberty can no more exist without virtue and independence than the body can live and move without a soul." - John Adams',
+			'[I]t is religion and morality alone which can establish the principles upon which freedom can securely stand. The only foundation of a free constitution is pure virtue." - John Adams',
+			'Agora and The Da Vinci Code are fiction, based on historical hoax.'
+		  ];
+
+injunList = [	'Carrier is the Ken Ham of Atheism; make that loud and clear as often as you can.',
+				'Don\'t these guys ever wonder why Carrier\'s not respected in Academia but only by uneducated anti-Christian atheists on the internet?',
+				'All Carrier does is revise history, even beyond his field of competence.',
+				'I have a new name for Carrier: the History Butcher.',
+				'The consensus of pretty much every expert in every relevant field is that Carrier’s stuff is, as far as their field is concerned, junk. It seems the only person whose expert opinion is that Carrier’s stuff isn’t junk is … Carrier (and his clueless acolytes).',
+				'I\'d say he\'s the worst [Carrier]. But whenever Aron Ra and Krauss open their mouths, it\'s hard to determine.',
+				'Krauss is the Kardashian of Atheism.',
+				'Dawkins opened up the gates of hell of ideological Anti-theism (after it was closed following the lack of interest in the movement that insane Madlyn O\'Haire).',
+				'I wouldn\'t be suprised if they start erecting statues of Dawkins, Hitchens, and them. Even Aron Ra has a bust already.'
+			];
+
+brassList = [	'I don\'t presuppose God but I presuppose my cognitive facilities are reliable and it just turns out God existing is the only way I can do that.',
+				'I love how Atheists do the whole root word argument for Agnostic when Agnostic was just a way to make fun of Gnostics.',
+				'The Old Testament has some beauty in it [...]. God showed a lot of generosity by going to men like Moses and Lot who all had there flaws and utterly didn\'t deserve their help at times.',
+				'What about TJ Kirk? Cheated on his wife (I believe with a guy), dropped out of high school, claimed to be smarter than Thomas Aquinas, made a porno vid with a bunch of people, etc.',
+				'Carrier is a pseudo historian who survives off his Atheism and not his actual scholarship.',
+				'I personally don\'t like debating anti vaxers, mythicists , Young Earth Creationists or Flat Earthers. I think all debating with them does is make them seem legitimate',
+				'I hate assholes, bad arguments, and bullying. It just so happens that a good part of the Atheist community I run into fit all 3 categories.'
+			];
